37b5ff48b024d697fe6314d8889645c19ae6b7c7
Aidan Bleser <aidanbleser35@gmail.com>
2024-07-12 16:17:21 -0500

    Update build.config.ts

--- build.config.ts
+++ build.config.ts
@@ -1,9 +1,9 @@
 import { defineBuildConfig } from 'unbuild';
 
 export default defineBuildConfig({
-	entries: ['src/index', 'src/prompts', 'src/template-files', 'src/util'],
+	entries: ['src/index.ts', 'src/prompts.ts', 'src/template-files.ts', 'src/util.ts'],
 	failOnWarn: false,
 	declaration: true,
 	clean: true,
-  outDir: "."
+	outDir: '.',
 });
