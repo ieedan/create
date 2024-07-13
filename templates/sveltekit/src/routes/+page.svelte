93aa93f2e2e66fbc8ac2b6369914925b31433728
Aidan Bleser <aidanbleser35@gmail.com>
2024-07-13 12:53:05 -0500

    Add header to template

--- templates/sveltekit/src/routes/+page.svelte
+++ templates/sveltekit/src/routes/+page.svelte
@@ -1,19 +1,26 @@
 <script lang="ts">
-    import { Button } from "$lib/components/ui/button";
+	import { Button } from '$lib/components/ui/button';
+	import LightSwitch from '$lib/components/ui/light-svelte/light-switch.svelte';
 </script>
 
-<main class="h-svh flex place-items-center justify-center px-8">
+<header class="flex place-items-center justify-center border-b px-6 py-2">
+	<div class="flex w-full max-w-6xl place-items-center justify-between">
+		<div></div>
+		<div>
+			<LightSwitch />
+		</div>
+	</div>
+</header>
+<main style="height: calc(100svh - 57px);" class="flex place-items-center justify-center px-8">
 	<div class="flex flex-col place-items-center justify-center gap-4">
-        <h1 class="text-4xl md:text-6xl text-center max-w-5xl">
-            <span class="font-serif bg-secondary px-1 py-1 rounded-md">
-                <span class="text-green-600">@iedan</span>/create
-            </span> 
-            SvelteKit Starter
-        </h1>
-        <div class="flex place-items-center gap-4">
-            <Button href="https://github.com/ieedan/create">
-                See Docs   
-            </Button>
-        </div>
-    </div>
+		<h1 class="max-w-5xl text-center text-4xl md:text-6xl">
+			<span class="rounded-md bg-secondary px-1 py-1 font-serif">
+				<span class="text-green-600">@iedan</span>/create
+			</span>
+			SvelteKit Starter
+		</h1>
+		<div class="flex place-items-center gap-4">
+			<Button href="https://github.com/ieedan/create">See Docs</Button>
+		</div>
+	</div>
 </main>
