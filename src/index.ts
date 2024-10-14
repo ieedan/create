4071637bb3de4459bebbf88819770dbcea22d090
Aidan Bleser <aidanbleser35@gmail.com>
2024-10-14 06:58:31 -0500

    fixed

--- src/index.ts
+++ src/index.ts
@@ -1616,6 +1616,9 @@ This project was created for you with the help of [template-factory](https://git
 											'../templates/ts-package/template-files/config.json',
 											import.meta.url
 										);
+
+										await fs.mkdir(path.join(dir, './.changeset'));
+
 										await fs.copyFile(
 											configJson,
 											path.join(dir, './.changeset/config.json')
